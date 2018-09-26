e5fd9c1fa2eaa220de308875d89050adb19eb25e
androidjia <781949152.com>
2018-09-26 16:25:14 +0800

    Initial commit

---
+++ app/src/main/java/ligong/com/dememeeem/MainActivity.java
@@ -0,0 +1,22 @@
+package ligong.com.dememeeem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int i =2;
+        switch (i){
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+        }
+    }
+}
